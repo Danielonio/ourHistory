c76e3739e3b92fae8aeafd0685c500ffdf490cc5
Daniel de la Peña <danieldelapena@X22087.local>
2023-05-26 00:03:03 +0200

    refactor: clean architecture initialization

---
+++ historical-character-creator/infraestructure/services/open_ai/client.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+class OpenAiCLient:
+  load_dotenv() 
+  api_key = os.environ.get('OPENAI_API_KEY')
+
+  headers = {
+      'Authorization': 'Bearer %s' % (api_key)
+  }
+
+  def post(url, data ):
+    post_response = requests.post(url, json=data, headers=OpenAiCLient.headers)
+    post_response_json = post_response.json()
+    return post_response_json

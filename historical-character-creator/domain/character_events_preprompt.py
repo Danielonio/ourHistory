8d78eac52a2c54c3c1f61f172d46968a763b3e3f
Daniel de la Peña <danieldelapena@X22087.local>
2023-05-26 15:37:37 +0200

    feat: preprompt

--- historical-character-creator/domain/character_events_preprompt.py
+++ historical-character-creator/domain/character_events_preprompt.py
@@ -1,1 +1,19 @@
-characterEventsPreprompt: str = 'Give me a brief summary about the life of ' 
+characterEventsPreprompt: str = '''
+I want to know 5 important key events in the life of a known historical character.
+Answer me in JSON format like so: 
+
+{"name": n,"events":  [{"date": d, "description": des, "place": p }}]}
+
+where n is the complete name of the character,
+d is the date in which the event took place in ISO format, 
+des is a description of the event the character was involved with 
+and p is the name of the place it happenned. p must be in JSON format with this properties
+name: n, coordinates: c, where n is the current name of the place, and c are the coordenates of the place in JSON format like so
+{"latitude": x, "longitude": y}.
+
+The events in the array should be ordered by the date field.
+The first event in the array has to be the day the character was born.
+The last event in the array has to be the day the character died.
+
+The name of the character you have to inform about is: 
+'''
